d9f45533a53c26e94bec08fe33ab47f12606d88e
fferrar1 <ferrarifernanda169@gmail.com>
2023-12-22 08:13:31 -0300

    criando modelos e campos do banco de dados

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+#o exemplo escolhido usa um app de enquetes, por isso temos Question, com a pergunta e a data de publicação; e Choice, com a "resposta" e o totalizador de conta dos votos
+class Question(models.Modrel):
+    question_text = models.CharField(max_length=200) #charfield informa que o campo selecionado é do tipo char
+    pub_date = models.DateTimeField("date published") #datetimefield informa ao banco de dados que o campo selecionado é do tipo datetime
 
-# Create your models here.
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200) #max_length é utilizado tanto no banco de dados quanto na futura validação
+    votes = models.IntegerField(default=0) #campo votes tipo Integer
