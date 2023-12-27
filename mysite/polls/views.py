50d44e1db071c0e53c267dc171e685ecd358a664
fferrar1 <ferrarifernanda169@gmail.com>
2023-12-27 10:48:31 -0300

    o teste de cliente da view e correção de erros

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,12 +8,12 @@ from django.views import generic
 from .models import Choice, Question
 
 class IndexView(generic.ListView):
-    templat_name = "polls/index.html"
+    template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objetcs.order_by("-pub_date")[:5]
+        return Question.objects.order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
