6d988d2a7f0e41fce96c8d387958dd744ba07298
fferrar1 <ferrarifernanda169@gmail.com>
2023-12-27 09:32:40 -0300

    corrigindo o erro e criando testes mais abrangentes

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,7 +9,8 @@ class Question(models.Model):
     def __str__(self): #facilita a representação do objeto tanto no prompt quanto em toda a interface
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -17,3 +18,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0) #campo votes tipo Integer
     def __str__(self):
         return self.choice_text
+    
