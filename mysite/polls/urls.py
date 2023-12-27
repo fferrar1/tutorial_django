a5831b254d3ba54886868029c17baac0efd96168
fferrar1 <ferrarifernanda169@gmail.com>
2023-12-27 08:15:07 -0300

    corrigindo erros e a URLconf

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"), #ex /polls/
-    path("specifics/<int:question_id>/", views.detail, name="detail"), #ex /polls/5/
-    path("<int:question_id>/results/", views.results, name="results"), #ex /polls/5/results
+    path("", views.IndexView.as_view(), name="index"), #ex /polls/
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"), #ex /polls/5/
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"), #ex /polls/5/results
     path("<int:question_id>/vote/", views.vote, name="vote"), # ex /polls/5/vote
 ]
 #o argumento de path:name é um poderoso aliado na hora de renomear urls;  path:view => Quando o Django encontra uma descrição que combina, chama a função view especificada com um objeto HttpRequest como primeiro argumento e qualquer valor “capturado” da rota como argumentos keyword.
