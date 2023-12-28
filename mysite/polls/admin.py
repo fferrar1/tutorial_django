2e9083ef93f4afce56224d5e9e32e8b79a3ed001
fferrar1 <ferrarifernanda169@gmail.com>
2023-12-28 08:00:20 -0300

    relacionando choice ao question na pagina de adm

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
+
+class ChoiceInLine(admin.TabularInline): #tabular vai apresentar os campos em forma de tabela, o stacked vai criar um formulário para cada choice
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date Information", {"fields": ["pub_date"]}),
     ]
+    inlines = [ChoiceInLine]
 
-admin.site.register(Question) #disponibiliza a Question criada na página do admin
-admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin) #disponibiliza a Question criada na página do admin
