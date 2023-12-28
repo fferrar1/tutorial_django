abb9ae6350bf90d411dfcdce69eb7458321f5312
fferrar1 <ferrarifernanda169@gmail.com>
2023-12-28 07:49:41 -0300

    personalizando a página do adm

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Question
 
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"]}),
+    ]
+
 admin.site.register(Question) #disponibiliza a Question criada na página do admin
